460113a646ccab7e162cc31c52fe1bf2284cffce
iVeluu <velualex10@gmail.com>
2025-05-15 18:44:36 -0600

    Alejandro L : pushing the first template

---
+++ navigation_app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const index = () => {
+  return (
+    <View className='mt-16'>
+      <Text className='text-3xl font-bold text-blue-500'>This is the first screen</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ navigation_app/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./App.tsx", 
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        'work-black': ['WorkSans-Black', 'sans-serif'],
+        'work-ligth': ['WorkSans-Light', 'sans-serif'],
+        'work-medium': ['WorkSans-Medium', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+};
